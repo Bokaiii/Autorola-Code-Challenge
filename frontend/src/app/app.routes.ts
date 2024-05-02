a34c87d4ba8c04e4ce9d30142e67253c843c766c
bokaiii <jakob1303@hotmail.com>
2024-05-02 20:55:02 +0200

    Added empty routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { CarOverviewComponent } from './car-overview/car-overview.component';
 
 export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'overview'},
   {path: 'overview', component: CarOverviewComponent}
 ];
